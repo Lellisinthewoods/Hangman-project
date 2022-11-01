0531c0ba4b6ff098138e14d42b2c7ba2a202b908
CarlMBergman <113527185+CarlMBergman@users.noreply.github.com>
2022-11-01 12:00:21 +0100

    nyuppladdning

--- hangman-boilerplate/js/index.js
+++ hangman-boilerplate/js/index.js
@@ -9,56 +9,52 @@
  */
 
 
-let words = [ //lista att hämta orden från!
-    {ord: `Puberteten`},{ord: `Irritation`},{ord: `Skallig`},
-    {ord: `Ljust`},{ord: `Murverk`},{ord: `Återförsäljare`},
-    {ord: `Auto`},{ord: `Kommunist`},{ord: `Division`},
-    {ord: `Mognad`},{ord: `Flocken`},{ord: `Nejlika`},
-    {ord: `Bajs`},{ord: `Turban`},{ord: `Radera`},
-    {ord: `Mascara`},{ord: `Sardinen`},{ord: `Rimfrost`},
-    {ord: `Brudgum`},{ord: `Ringa`},{ord: `Tid`},
-    {ord: `Fiske`},{ord: `Rekord`},{ord: `Turism`},
-    {ord: `Alarm`},{ord: `Senaste`},{ord: `Sfär`},
-    {ord: `Hägring`},{ord: `Uniform`},{ord: `Zoo`},
-  ]
+ let words = [ //lista att hämta orden från!
+ {ord: `Puberteten`},{ord: `Irritation`},{ord: `Skallig`},
+ {ord: `Ljust`},{ord: `Murverk`},{ord: `Återförsäljare`},
+ {ord: `Auto`},{ord: `Kommunist`},{ord: `Division`},
+ {ord: `Mognad`},{ord: `Flocken`},{ord: `Nejlika`},
+ {ord: `Bajs`},{ord: `Turban`},{ord: `Radera`},
+ {ord: `Mascara`},{ord: `Sardinen`},{ord: `Rimfrost`},
+ {ord: `Brudgum`},{ord: `Ringa`},{ord: `Tid`},
+ {ord: `Fiske`},{ord: `Rekord`},{ord: `Turism`},
+ {ord: `Alarm`},{ord: `Senaste`},{ord: `Sfär`},
+ {ord: `Hägring`},{ord: `Uniform`},{ord: `Zoo`},
+]
 
 let word = `word`;
 
+
 document.addEventListener(`keypress`, (e) => { //lyssnar efter event från tangentbordsknappar
-   console.log(`hej hej ${e.key}`)
-    for (let i = 0; i < word.length; i++) {
-        if (e.key == word[i]) {
-        console.log(`rätt på plats ${i}`)
-        }
-        else {
-            console.log(`${e.key}`)
-            
-        }
-    }
+let letter = e.key;
+
+
+
+ for (let i = 0; i < word.length; i++) {
+     if (letter === word[i]) {
+
+     }
+     else {
+        
+
+     }
+ }
 })
+
+function randomizer(){
+ let randomNumber = Math.floor(Math.random()*words.length);
+ word = words.splice(randomNumber, 1)
+ console.log(word[0].ord)
+ let streck = document.querySelector(`h6`) 
+ streck.innerText = ``;
+ for (let i = 0; i < word[0].ord.length; i++ ) {
+    streck.innerText += (' _ ') 
+
+}
+}
 
 document.querySelector(`.randomizerButton`) //knapp för att slumpa fram ord ur listan
 .addEventListener(`click`, ()=>{
-   let randomNumber = Math.floor(Math.random()*words.length);
-   word = words.splice(randomNumber, 1)
-   console.log(word[0].ord)
-   let streck = document.querySelector(`h6`) 
-   streck.innerText = ``;
-   for (let i = 0; i < word[0].ord.length; i++ ) {
-      streck.innerText += (' _ ') 
-      console.log(word[0].ord.length)
-   }
-})
-
-
-    //Reset knappen
-    let resetButton= document.querySelector(`.resetButton`)
-
-    function toggle() {
-        slider.classList.toggle("show");
-      }
-      resetButton.addEventListener("click", () => {
-        let resetButton = Math.floor(Math.random()*words.length);
-        word=words.splice (resetButton, 1)
-
-      });
+randomizer()
+}
+)
