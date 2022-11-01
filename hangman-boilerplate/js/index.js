5988ddebae28c7ab0d9063b39d4e7e336d62b1f3
NajmaHasan <114648473+NajmaHasan@users.noreply.github.com>
2022-11-01 09:29:31 +0100

    slump

--- hangman-boilerplate/js/index.js
+++ hangman-boilerplate/js/index.js
@@ -7,3 +7,30 @@
  document.querySelector('figure').classList.add('legs')
 
  */
+
+ let words = [
+  {ord: `Puberteten`},{ord: `Irritation`},{ord: `Skallig`},
+    {ord: `Ljust`},{ord: `Murverk`},{ord: `Återförsäljare`},
+    {ord: `Auto`},{ord: `Kommunist`},{ord: `Division`},
+    {ord: `Mognad`},{ord:`Flocken`},{ord:`Nejlika`},
+    {ord: `Loop`},{ord: `Turban`},{ord: `Radera`},
+    {ord: `Mascara`},{ord: `Sardinen`},{ord: `Rimfrost`},
+    {ord: `Brudgum`},{ord: `Ringa`},{ord: `Tid`},
+    {ord: `Fiske`},{ord: `Rekord`},{ord:`Turism`},
+    {ord: `Alarm`},{ord: `Senaste`},{ord:`Sfär`},
+    {ord:`Hägring`},{ord:`Uniform`},{ord:`Zoo`},
+ ]
+
+ 
+
+
+document.querySelector(`.randomizerButton`)
+.addEventListener(`click`, ()=>{
+ let word = Math.floor(Math.random()*words.length);
+console.log(word);
+})
+
+
+
+
+ 
