4d275fc252c0ed9cdd730d96d6a7bdc8ccfbdc31
Lellisinthewoods <ellinor.romin@hotmail.com>
2022-11-03 10:06:49 +0100

    added timer test

--- hangman-boilerplate/js/index.js
+++ hangman-boilerplate/js/index.js
@@ -143,4 +143,29 @@ function compareLetters(userLetter)
     return bool;
 }
 
+ 
+       function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            alert(`TIDEN ÄR UTE SUCKAAAAAAH`);
+            timer = duration;
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fiveMinutes = 60 * 1,
+        display = document.querySelector('header');
+    startTimer(fiveMinutes, display);
+};
+
 window.onload = randomizer()
